14320860f9b6ebf634b806101792df8391dff266
Maksim <maksim.palii@gmail.com>
2020-09-14 21:20:46 +0300

    first commit

---
+++ js/sliders.js
@@ -0,0 +1,13 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
